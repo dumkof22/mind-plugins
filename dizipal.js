9fa03a1b105804d21d5048c88ed32889e53a64ef
dumkof22 <102432925+dumkof22@users.noreply.github.com>
2025-11-29 23:38:05 +0300

    Update dizipal.js

--- dizipal.js
+++ dizipal.js
@@ -141,7 +141,7 @@ const manifest = {
     idPrefixes: ['dizipal']
 };
 
-const BASE_URL = 'https://dizipal1219.com';
+const BASE_URL = 'https://dizipal1220.com';
 
 // ============ BOT DETECTION PREVENTION ============
 // Delay helper function
