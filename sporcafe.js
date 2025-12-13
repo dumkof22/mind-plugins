e53b78969248d96708699aa677050830b7643721
dumkof22 <102432925+dumkof22@users.noreply.github.com>
2025-12-13 11:22:42 +0300

    Update sporcafe.js

--- sporcafe.js
+++ sporcafe.js
@@ -63,7 +63,7 @@ const DYNAMIC_BASE_URLS = [
 ];
 
 // Varsayılan URL (dinamik URL bulunamazsa kullanılacak)
-let BASE_URL = 'https://www.selcuksportshdb7d4c8a075.xyz';
+let BASE_URL = 'https://www.selcuksportshdedd967562e.xyz';
 const PLAYER_BASE_URL = 'https://main.uxsyplayerb03b3c895b.click/index.php?id=';
 
 // Dinamik URL'i güncelle (Flutter tarafından çağrılacak)
